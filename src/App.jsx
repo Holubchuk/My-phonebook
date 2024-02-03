28d756ef686981d1256447f79b28610887348f38
Holubchuk <sashagolubchuk3@gmail.com>
2024-02-02 23:40:57 -0500

    feature/basic-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-// import { AddContactForm } from './components/AddContactForm/AddContactForm';
-// import { SearchFilter } from './components/SearchFilter/SearchFilter';
-// import { ContactsList } from './components/ContactsList/ContactsList';
 import { Suspense, lazy, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Layout } from 'components/Layout/Layout';
@@ -57,14 +54,3 @@ export const App = () => {
       </Layout>
     );
 };
-
-
-
-{/* <div>
-<h2 style={{ textAlign: 'center' }}>Phonebook</h2>
-<AddContactForm />
-
-<h2 style={{ textAlign: 'center' }}>Contacts</h2>
-<SearchFilter />
-<ContactsList />
-</div> */}
